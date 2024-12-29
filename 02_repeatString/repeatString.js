75db10070ceeabeceeb5e69d8bba4588731d28b0
Aditya Anande <98577304+aditya-anande@users.noreply.github.com>
2024-12-29 13:52:06 +0530

    2nd exercise completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,n) {
+let repeatedString="";
+if(n<0) {
+    return "ERROR";
+}
+while(n>0) {
+    repeatedString += string;
+    n--;
+}
+return repeatedString;
 };
 
 // Do not edit below this line
