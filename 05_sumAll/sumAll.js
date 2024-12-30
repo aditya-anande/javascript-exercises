77e3ef907c301726be7f471b38165e341a4e062a
Aditya Anande <98577304+aditya-anande@users.noreply.github.com>
2024-12-30 14:35:04 +0530

    fifth exercise completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    if(start<0 || end<0) return "ERROR";
+    if(!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    if(start>end) {
+        const temp=start; //start value in temp
+        start=end; //end value in start
+        end=temp; //start value in end  
+    }
+    let sum=0;
+    for(let i=start;i<=end;i++) {
+        sum=sum+i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
