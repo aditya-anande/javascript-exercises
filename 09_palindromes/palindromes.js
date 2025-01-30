751d62ac833616bac949faa5a0576821ef6ff841
Aditya Anande <98577304+aditya-anande@users.noreply.github.com>
2025-01-30 16:39:08 +0530

    completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanedString=str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reversedString=cleanedString.split("").reverse().join("");
+    return cleanedString===reversedString;
 };
 
 // Do not edit below this line
