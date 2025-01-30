1c10190bd2a0d882984c57106236cd0ceddd5901
Aditya Anande <98577304+aditya-anande@users.noreply.github.com>
2025-01-30 17:56:51 +0530

    completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const age=function(birth,death) {
+    if(!death) {
+        death=new Date().getFullYear();
+    }
+    return death-birth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest,currentPerson)=>{
+        const oldestAge=age(oldest.yearOfBirth,oldest.yearOfDeath);
+        const currentAge=age(currentPerson.yearOfBirth,currentPerson.yearOfDeath);
+        return oldestAge<currentAge ? currentPerson:oldest;
+    });
 };
 
 // Do not edit below this line
