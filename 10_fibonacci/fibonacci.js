c3a36fda1bd6b08e2d7e95e281b318ef9a8f4c76
Aditya Anande <98577304+aditya-anande@users.noreply.github.com>
2025-01-30 16:48:17 +0530

    completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+let num;
+if(typeof n!== "number"){
+    num=parseInt(n);
+} else {
+    num=n;
+}
+
+if(num<0) return("OOPS"); //No negative allowed
+if(num===0) return(0);
+
+let a=0,b=1;
+for(let i=2;i<=n;i++) {
+    let temp=a+b;
+    a=b;
+    b=temp;
+}
+return b;
 };
 
 // Do not edit below this line
